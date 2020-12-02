db7ecbf9058798794bae83655823e6d338a70603
FPTheFluffyPawed <n.ito10@hotmail.com>
2020-12-02 18:44:52 +0000

    Add TMP and Interaction base
    Added the baseline for interaction, have to develop on proper interactions still but should work for now.

---
+++ Assets/Scripts/Interactive.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactive : MonoBehaviour, Interactable
+{
+    [SerializeField] private string interactiveName;
+
+    private void Start()
+    {
+        
+    }
+
+    public string ExamineText()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/UserInterface.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterface : MonoBehaviour
+{
+    public Image Crosshair { get; private set; }
+}
