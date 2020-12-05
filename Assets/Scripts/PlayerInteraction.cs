68c2a61a699e66d638d3b1c0645efb8062dd5024
FPTheFluffyPawed <n.ito10@hotmail.com>
2020-12-05 15:15:01 +0000

    Add Interactive highlight
    Feedback added whenever the player finds an interactive object now.

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -6,23 +6,72 @@ public class PlayerInteraction : MonoBehaviour
 {
     [SerializeField] float interactionDistance = 2.0f;
 
-    private Camera cam;
-    private UserInterface ui;
-    private Ray ray;
+    private Camera _cam;
+    private UserInterface _ui;
+    private Interactive _currentInteractive;
 
     private void Start()
     {
-        cam = GetComponentInChildren<Camera>();
-        ui = GetComponentInChildren<UserInterface>();
+        _cam = GetComponentInChildren<Camera>();
+        _ui = GetComponentInChildren<UserInterface>();
+        _currentInteractive = null;
     }
 
     private void FixedUpdate()
     {
-        if(Physics.Raycast(cam.transform.position, cam.transform.forward, interactionDistance))
-        {
-            Debug.DrawRay(cam.transform.position, transform.TransformDirection(cam.transform.forward * interactionDistance), Color.yellow);
-            Debug.Log("Hit");
+        CheckForInteractive();
+        Hovering();
+    }
 
+    private void Hovering()
+    {
+        if (_currentInteractive != null)
+            _currentInteractive.Highlight();
+    }
+
+    /// <summary>
+    /// Check for any Interactive GOs in the Player's raycast.
+    /// </summary>
+    private void CheckForInteractive()
+    {
+        if (Physics.Raycast(_cam.transform.position,
+                            _cam.transform.forward,
+                            out RaycastHit hit,
+                            interactionDistance))
+        {
+            Interactive newInteractive = hit.collider.GetComponent<Interactive>();
+
+            if (newInteractive != null && newInteractive != _currentInteractive)
+                SetCurrentInteractive(newInteractive);
+            else if (newInteractive == null)
+                ClearCurrentInteractive();
         }
+        else
+            ClearCurrentInteractive();
+    }
+
+    private void SetCurrentInteractive(Interactive newInteractive)
+    {
+        _currentInteractive = newInteractive;
+    }
+
+    private void ClearCurrentInteractive()
+    {
+        _currentInteractive = null;
+    }
+
+    private void Pickup()
+    {
+
+    }
+
+    private void Talk()
+    {
+
+    }
+
+    private void Interact()
+    {
+        _currentInteractive.Interact();
     }
 }
