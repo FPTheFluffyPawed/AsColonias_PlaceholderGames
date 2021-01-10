f1eff3d54b3f47da17e937e24cc381bc7a1e70d9
Inácio Amerio <n.ito10@hotmail.com>
2021-01-10 16:32:24 -0300

    Add scene loading base
    Base test for creating our loading screens.

---
+++ Assets/IA_Assets/LoadLevel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        StartCoroutine(LoadNextScene());
+    }
+
+
+    IEnumerator LoadNextScene()
+    {
+        yield return null;
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(1);
+        asyncOperation.allowSceneActivation = false;
+
+        while(!asyncOperation.isDone)
+        {
+            if(asyncOperation.progress >= 0.9f)
+            {
+                Debug.Log("Finished loading");
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                    asyncOperation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
