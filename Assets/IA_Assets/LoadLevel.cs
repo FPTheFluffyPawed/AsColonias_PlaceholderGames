7ddd63fef7ff22d1cc6847603b30f867c331b58e
Inácio Amerio <n.ito10@hotmail.com>
2021-01-12 15:45:26 -0300

    Add load screen animation and UI
    Fade in animation and other elements for a base style.

--- Assets/IA_Assets/LoadLevel.cs
+++ Assets/IA_Assets/LoadLevel.cs
@@ -2,19 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class LoadLevel : MonoBehaviour
 {
+    private Animator _animator;
+
+    [SerializeField] private TextMeshProUGUI _continueText;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     private void OnEnable()
     {
         StartCoroutine(LoadNextScene());
     }
 
-
     IEnumerator LoadNextScene()
     {
-        yield return null;
+        yield return new WaitForSeconds(10);
 
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(1);
         asyncOperation.allowSceneActivation = false;
@@ -23,6 +31,8 @@ public class LoadLevel : MonoBehaviour
         {
             if(asyncOperation.progress >= 0.9f)
             {
+                _continueText.gameObject.SetActive(true);
+
                 Debug.Log("Finished loading");
 
                 if (Input.GetKeyDown(KeyCode.Space))
