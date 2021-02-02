e0d9b2c10af3d930685f867aa9eac68655f1f99e
FPTheFluffyPawed <n.ito10@hotmail.com>
2021-02-02 19:07:51 +0000

    New scripts for prototype

---
+++ Assets/Scripts/EatConversation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatConversation : MonoBehaviour
+{
+    [SerializeField] private GameObject[] enableDisableGameObjects;
+    [SerializeField] private GameObject player;
+    [SerializeField] private AudioClip sampaioClip;
+    [SerializeField] private TextAsset eatSubtitles;
+
+    private void UpdateSampaioDialog()
+    {
+        player.GetComponent<AudioSource>().clip = sampaioClip;
+        player.GetComponent<AudioSource>().Play();
+    }
+
+    private void StartConversation()
+    {
+        player.GetComponent<PlayerMovement>().enabled = false;
+        player.GetComponentInChildren<PlayerInterface>().DisplaySubtitles(eatSubtitles);
+
+    }
+
+    private void TransitionOutOfConversation()
+    {
+        player.GetComponent<PlayerMovement>().enabled = true;
+
+        for(int i = 0; i < enableDisableGameObjects.Length; i++)
+        {
+            if (enableDisableGameObjects[i].activeSelf)
+                enableDisableGameObjects[i].SetActive(false);
+            else
+                enableDisableGameObjects[i].SetActive(true);
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/FormationTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormationTrigger : MonoBehaviour
+{
+    [SerializeField] private AudioSource soldier;
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private SubtitleDisplayer playerSubtitles;
+    [SerializeField] private TextAsset newSubtitles;
+    [SerializeField] private GameObject[] enableDisableGameObjects;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            soldier.Play();
+            playerSubtitles.Subtitle = newSubtitles;
+            StartCoroutine(WaitForOtherCoroutine());
+        }
+    }
+
+    private IEnumerator WaitForOtherCoroutine()
+    {
+        playerMovement.enabled = false;
+        yield return StartCoroutine(playerSubtitles.Begin());
+        playerMovement.enabled = true;
+
+        for(int i = 0; i < enableDisableGameObjects.Length; i++)
+        {
+            if (enableDisableGameObjects[i].activeSelf)
+                enableDisableGameObjects[i].SetActive(false);
+            else
+                enableDisableGameObjects[i].SetActive(true);
+        }
+
+        Destroy(gameObject);
+    }
+}
