422c67c742f821aa54fbb2d04bfeccb4d15479d3
FPTheFluffyPawed <n.ito10@hotmail.com>
2021-02-03 19:54:42 +0000

    Final changes for prototype

--- Assets/IA_Assets/LoadLevel.cs
+++ Assets/IA_Assets/LoadLevel.cs
@@ -7,6 +7,7 @@ using TMPro;
 public class LoadLevel : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _continueText;
+    [SerializeField] private TextMeshProUGUI _narrationText;
 
     private void OnEnable()
     {
@@ -26,6 +27,7 @@ public class LoadLevel : MonoBehaviour
             {
                 Debug.Log("Finished loading");
 
+                _narrationText.gameObject.SetActive(true);
                 _continueText.gameObject.SetActive(true);
 
                 if (Input.GetKeyDown(KeyCode.Space))
---
+++ Assets/Scripts/TypewriterEffect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// Obtained online through UnityCoder.
+/// https://unitycoder.com/blog/2015/12/03/ui-text-typewriter-effect-script/
+/// </summary>
+public class TypewriterEffect : MonoBehaviour
+{
+    private TextMeshProUGUI _text;
+    private string _narration;
+
+    private void Awake()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        _narration = _text.text;
+        _text.text = null;
+
+        StartCoroutine(PlayText());
+    }
+
+    IEnumerator PlayText()
+    {
+        foreach(char c in _narration)
+        {
+            _text.text += c;
+            yield return new WaitForSeconds(0.050f);
+        }
+    }
+}
